02c0e57c8f6ec2ee0d3b6bfc2dcab4e2ce4b483b
Michael Herndon <mherndon@Michael-Herndons-MacBook-Pro.local>
2010-03-04 17:46:38 -0500

    fixed some navigation qirks as well as some various styling issues in IE 7 for the pagination and percentages for the reports in the sidebar

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,7 +4,6 @@ class PatientsController < ApplicationController
 
 
   before_filter :initialize_reports
-  skip_before_filter :login_required
   layout  "site"
   
   def initialize_reports
@@ -21,7 +20,25 @@ class PatientsController < ApplicationController
     
     #TODO: abstract this out into a module for active record. 
     if @report_id.nil? || @report_id == ""
-      @list = UI::PaginatedResults.wrap(:patients, params) { |options|
+    
+      query = Report.find_and_generate_patients_query(params[:report_id], "")
+      if !query.nil?
+        @report_id = params[:report_id]
+        @list = UI::PaginatedResults.wrap(:patients, params) { |options|
+          count = Patient.count_by_sql("SELECT COUNT( DISTINCT patients.id) FROM patients #{query} ")
+        
+            if(count > 0 && options[:offset] > count)
+              options[:offset] = count - options[:limit]
+            end
+        
+          {:data => Patient.find_by_sql("SELECT patients.* FROM patients #{query} LIMIT #{options[:limit]} OFFSET #{options[:offset]}"), :count => count}
+        }
+        @ret = @ret + "/report/" + @report_id.to_s
+        render 
+      end
+    end
+    
+     @list = UI::PaginatedResults.wrap(:patients, params) { |options|
           count = Patient.count
  
           if(count > 0 && options[:offset] > count)
@@ -36,41 +53,28 @@ class PatientsController < ApplicationController
           @ret = @ret + "/report/" + first.id.to_s
       end
     
-    else 
-      query = Report.find_and_generate_patients_query(params[:report_id], "")
-      @report_id = params[:report_id]
-      @list = UI::PaginatedResults.wrap(:patients, params) { |options|
-        count = Patient.count_by_sql("SELECT COUNT( DISTINCT patients.id) FROM patients #{query} ")
-        
-          if(count > 0 && options[:offset] > count)
-            options[:offset] = count - options[:limit]
-          end
-        
-        {:data => Patient.find_by_sql("SELECT patients.* FROM patients #{query} LIMIT #{options[:limit]} OFFSET #{options[:offset]}"), :count => count}
-      }
-      @query = params[:query]
-      
-      
-      @ret = @ret + "/report/" + @report_id.to_s
-    end
-    
     return render 
   end
   
   # GET
   def show
     redirect = false
-    id = params[:id] || params[:report_id]
+    id =  params[:id]
+        
+    @report_id = params[:report_id]|| ""
+   
+     if @report_id == ""
+        first = Report.first;
+        if !first.nil?
+          @report_id = first.id.to_s
+        end
+      end
     
-    @report_id = id || ""
+    @return = CGI.unescape(params[:return])
+    @ret =  (params[:return].nil? ? "/patients/list/?" : "#{@return}&amp;")
+    @ret = @ret + "report_id=" + @report_id
     
-    @ret = "/patients/list/" 
-    @ret = @ret + (params[:return].nil? ? "?" : "return=#{params[:return]}&amp;")
-    @ret = @ret + "report_id=" + CGI.escape(@report_id)
-    
-    if @report_id == ""
-      @report_id = Report.first.id.to_s
-    end
+   
     
     
     if id.nil? || id.to_i <= 0
