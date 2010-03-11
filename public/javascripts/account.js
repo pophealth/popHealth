5db24c06f20a1df9a450f283ab0ec570425a9c21
I. Shauni Deshmukh <ideshmukh@ideshmukh-macbookpro.local>
2010-03-11 16:49:34 -0500

    fixes to account buttons and fonts

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,10 +1,56 @@
-	// select all forms, bind keypress, and if enter was pushed, submit the form.
-	(function(){
-		$(function(){
-			$("form").keypress(function(e){
-				var code = e.keyCode || e.which;
-				if(code == 13)
-					this.submit();
-			});
+function submitKeyPress(e) {
+	var keynum;
+	if(window.event) 
+	{
+		keynum = e.keyCode;
+	}
+	else if(e.which)
+	{
+		keynum = e.which;
+	}
+	else return true;
+
+	if (keynum == 13)
+	{
+		document.laika_login_form.submit();
+		return false;
+	}
+	else
+	return true;
+}
+
+function submitLaikaLoginform() {      
+	document.laika_login_form.submit(); 
+}
+
+function cancelLogin() {
+	document.laika_login_form.reset();
+}
+
+function submitLaikaAccountCreationform() {      
+	document.laika_account_creation_form.submit(); 
+}
+
+function submitResetPasswordinform() {
+	document.laika_reset_form.submit(); 
+}
+
+function wait(msecs) {
+	var start = new Date().getTime();
+	var cur = start
+	while(cur - start < msecs)
+	{
+		cur = new Date().getTime();
+	}		
+}
+
+// select all forms, bind keypress, and if enter was pushed, submit the form.
+(function(){
+	$(function(){
+		$("form").keypress(function(e){
+			var code = e.keyCode || e.which;
+			if(code == 13)
+			this.submit();
 		});
+	});
 	})();
