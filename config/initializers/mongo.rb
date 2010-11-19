2b197ccaab467332a5732c93b5f6e20c70eb3984
Andy Gregorowicz <andy@mitre.org>
2010-11-19 07:59:15 -0500

    Using the URL with username and passowrd in it

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -2,7 +2,7 @@ require 'mongo'
 
 if ENV['MONGOHQ_URL']
   uri = URI.parse(ENV['MONGOHQ_URL'])
-  conn = Mongo::Connection.new(uri.host, uri.port)
+  conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
   MONGO_DB = conn.db(uri.path.gsub(/^\//, ''))
 else
   MONGO_DB = Mongo::Connection.new('localhost', 27017).db('test')
