7c173800a85a853b24e1fcad0c287071eb79ec28
Andy Gregorowicz <andy@mitre.org>
2010-12-13 15:44:10 -0500

    Adjusting the MongoDB initializer to allow for a connection to a test db

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -4,6 +4,8 @@ if ENV['MONGOHQ_URL']
   uri = URI.parse(ENV['MONGOHQ_URL'])
   conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
   MONGO_DB = conn.db(uri.path.gsub(/^\//, ''))
+elsif ENV['TEST_DB_HOST']
+  MONGO_DB = Mongo::Connection.new(ENV['TEST_DB_HOST'], 27017).db("pophealth-#{Rails.env}")
 else
   MONGO_DB = Mongo::Connection.new('localhost', 27017).db("pophealth-#{Rails.env}")
 end
