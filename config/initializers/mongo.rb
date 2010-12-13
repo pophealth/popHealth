99fd9f9accfc28c4ad18265695dc6d3d583c9765
Andy Gregorowicz <andy@mitre.org>
2010-12-13 13:28:36 -0500

    Moving the MongoDB out of just 'test' and basing it on environment

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -5,7 +5,7 @@ if ENV['MONGOHQ_URL']
   conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
   MONGO_DB = conn.db(uri.path.gsub(/^\//, ''))
 else
-  MONGO_DB = Mongo::Connection.new('localhost', 27017).db('test')
+  MONGO_DB = Mongo::Connection.new('localhost', 27017).db("pophealth-#{Rails.env}")
 end
 
 QME::MongoHelpers.initialize_javascript_frameworks(MONGO_DB)
