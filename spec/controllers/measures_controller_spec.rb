0505f5572b2ecb6975289a48899f68563231b9bb
Andy Gregorowicz <andy@mitre.org>
2010-12-15 16:22:23 -0500

    Better testing of the MeasuresController

--- spec/controllers/measures_controller_spec.rb
+++ spec/controllers/measures_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe MeasuresController do
   before do
-    collection_fixtures 'measures'
+    collection_fixtures 'measures', 'selected_measures', 'records'
   end
   
   describe "GET 'definition'" do
@@ -10,6 +10,15 @@ describe MeasuresController do
       get :definition, :id => '0013'
       response.should be_success
       assigns(:definition).should_not be_nil
+    end
+  end
+  
+  describe "GET 'index'" do
+    it "should be successful" do
+      get :index
+      response.should be_success
+      assigns(:patient_count).should == 2
+      assigns(:grouped_selected_measures).should have_key("Women's Health")
     end
   end
   
