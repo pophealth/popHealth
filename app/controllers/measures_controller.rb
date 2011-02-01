761185a174d31d0704dcc8751ca868c4d865116c
Andy Gregorowicz <andy@mitre.org>
2011-02-01 15:51:27 -0500

    Hardcoding effective_date in one place

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,7 +1,7 @@
 class MeasuresController < ApplicationController
   include RailsWarden::Mixins::HelperMethods
   include RailsWarden::Mixins::ControllerOnlyMethods
-  before_filter :set_up_executor
+  before_filter :set_up_environment
   before_filter :authenticate!
   
   def index
@@ -14,7 +14,7 @@ class MeasuresController < ApplicationController
   end
     
   def result
-    @result = @executor.measure_result(params[:id], params[:sub_id], :effective_date=>Time.gm(2010, 12, 31).to_i)
+    @result = @executor.measure_result(params[:id], params[:sub_id], :effective_date=> @effective_date)
     
     render :json => @result
   end
@@ -32,47 +32,42 @@ class MeasuresController < ApplicationController
   end
   
   def patients
-    @result = @executor.measure_result(params[:id], params[:sub_id], :effective_date=>Time.gm(2010, 12, 31).to_i)
+    @result = @executor.measure_result(params[:id], params[:sub_id], :effective_date=> @effective_date)
     @definition = @executor.measure_def(params[:id], params[:sub_id])
-    
-   # @numerator = mongo['records'].find('_id' => {'$in' => @result[:numerator_members]})
-   # @anti_numerator = mongo['records'].find('_id' => {'$in' => @result[:antinumerator_members]})
   end
   
-  
-  
   def measure_patients
-
-    @result = @executor.measure_result(params[:id], params[:sub_id], :effective_date=>Time.gm(2010, 12, 31).to_i)
+    @result = @executor.measure_result(params[:id], params[:sub_id], :effective_date=> @effective_date)
     type = params[:type] || "denominator"
    
     @limit = (params[:limit] || 20).to_i
-    @skip =(( params[:page] || 1).to_i - 1 ) * @limit
+    @skip = ((params[:page] || 1).to_i - 1 ) * @limit
     sort = params[:sort] || "_id"
     sort_order = params[:sort_order] || :asc
     measure_id = params[:id] 
     sub_id = params[:sub_id]
-    effective_date = ( params[:effective_date] || Time.gm(2010, 12, 31)).to_i
-    cache_name =  "cached_measure_patients_#{measure_id}_#{sub_id}_#{effective_date}"
-    @records = mongo[cache_name].find({:measure_id=>measure_id,:sub_id=>sub_id,:effective_date=>effective_date,type=>true},{:sort=>[sort, sort_order],:skip=>@skip,:limit=>@limit}).to_a
-    @total =  mongo[cache_name].find({:measure_id=>measure_id,:sub_id=>sub_id,:effective_date=>effective_date,type=>true}).count
-    
+    cache_name =  "cached_measure_patients_#{measure_id}_#{sub_id}_#{@effective_date}"
+    @records = mongo[cache_name].find({:measure_id => measure_id, :sub_id => sub_id,
+                                       :effective_date => @effective_date, type => true}, 
+                                      {:sort => [sort, sort_order], :skip => @skip, :limit => @limit}).to_a
+    @total =  mongo[cache_name].find({:measure_id => measure_id, :sub_id => sub_id,
+                                      :effective_date => @effective_date, type => true}).count
+
     @page_results = WillPaginate::Collection.create((params[:page] || 1), @limit, @total) do |pager|
        pager.replace(@records)
     end
- 
+
   end
-  
-  
+
   def select
     measure = Measure.add_measure(params[:id])
     
     results = {:patient_count => mongo['records'].count}
     if measure['subs'].empty?
-      results[measure['id']] = @executor.measure_result(params[:id], nil, :effective_date=>Time.gm(2010, 12, 31).to_i)
+      results[measure['id']] = @executor.measure_result(params[:id], nil, :effective_date=> @effective_date)
     else
       measure['subs'].each do |sub_id|
-        results[measure['id'] + sub_id] = @executor.measure_result(params[:id], sub_id, :effective_date=>Time.gm(2010, 12, 31).to_i)
+        results[measure['id'] + sub_id] = @executor.measure_result(params[:id], sub_id, :effective_date=> @effective_date)
       end
     end
     render :partial => 'measure_stats', :locals => {:measure => measure, :results => results}
@@ -85,9 +80,11 @@ class MeasuresController < ApplicationController
 
   private
   
-  def set_up_executor
+  def set_up_environment
     @executor = QME::MapReduce::Executor.new(mongo)
     @patient_count = mongo['records'].count
+    
+    @effective_date = Time.gm(2010, 12, 31).to_i
   end
 
 end
