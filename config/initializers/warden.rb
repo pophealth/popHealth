5d30653025f45789fa3de16281949245da99fa28
Andy Gregorowicz <andy@mitre.org>
2011-02-02 09:08:05 -0500

    Whitespace cleanup

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,42 +1,39 @@
 Rails.configuration.middleware.use RailsWarden::Manager do |manager|
-    manager.default_strategies :my_strategy
-    manager.failure_app = LoginController
+  manager.default_strategies :my_strategy
+  manager.failure_app = LoginController
+end
+
+# Setup Session Serialization
+class Warden::SessionSerializer
+  def serialize(record)
+    [record._id]
   end
 
-  # Setup Session Serialization
-  class Warden::SessionSerializer
-    def serialize(record)
-      [record._id]
-    end
+  def deserialize(keys)
+     id = keys[0]
+     User.find_one({'_id' => id})
+  end
+end
 
-    def deserialize(keys)
-       id = keys[0]
-       User.find_one({'_id' => id})
-    end
+# Declare your strategies here
+Warden::Strategies.add(:my_strategy) do
+
+  def valid?
+    params["username"] || params["password"]
   end
 
-  # Declare your strategies here
-  Warden::Strategies.add(:my_strategy) do
-    
-    def valid?
-        params["username"] || params["password"]
+  def authenticate!
+    user = User.authenticate(params[:username],params[:password])
+
+    if user && user.verified
+      success!(user)
+    elsif user
+      errors.add(:login, "Account not yet verified")
+      fail!
+    else
+      errors.add(:login, "Username or Password incorrect")
+      fail!
     end
-      
-   def authenticate!
-          user = User.authenticate(params[:username],params[:password])
-          
-          if user && user.verified
-            success!(user)
-          elsif user
-            errors.add(:login, "Account not yet verified")
-            fail!
-          else
-             errors.add(:login, "Username or Password incorrect")
-             fail!
-          end
-   end 
   end
-  
-  
-  
+end
   
