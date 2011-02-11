f22a8c617fbf6e223f32ae6712238c7d8ab639a9
Rob McCready <rob@mitre.org>
2011-02-11 18:12:17 -0500

    Code formatting cleanup with the Users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 require 'uniq_validator'
 
 class User < MongoBase
-  
-  
+
   def self.add_delegate(key)
     define_method(key) do
       read_attribute_for_validation(key)
     end
-    
     define_method("#{key.to_s}=".to_sym) do |val|
       set_attribute_value(key, val)
     end
   end
-  
+
   add_delegate :password
   add_delegate :username
   add_delegate :first_name
@@ -20,13 +18,13 @@ class User < MongoBase
   add_delegate :email
   add_delegate :company
   add_delegate :company_url
-  
+
   add_delegate :locked
   add_delegate :reset_key
   add_delegate :_id
-  
+
   include ActiveModel::Validations
-  
+
   validates_presence_of :first_name, :last_name
   validates :email, :presence => true, 
                     :length => {:minimum => 3, :maximum => 254},
@@ -47,17 +45,15 @@ class User < MongoBase
         return User.new(u)
       end
     end
-    
     return nil
   end
-
 
   # See if the username already exists
   # param [String] username
   def self.check_username(username)
     mongo['users'].find_one({:username => attributes[:username]})
   end
-  
+
   # Find users based on hash of key value pairs
   # param [Hash] params key value pairs to use as a filter - same as would be passed to mongo collection
   def self.find(params)
@@ -70,17 +66,17 @@ class User < MongoBase
     atts = mongo['users'].find_one(params)
     atts ? User.new(atts) : nil
   end
-  
+
   def initialize(attributes = {})
     @attributes = attributes || {}
   end
-  
+
   # get the value for a field
   # param [String || Symbol] key the value to get
   def read_attribute_for_validation(key)
     @attributes[key.to_sym] || @attributes[key.to_s]
   end
-  
+
   # Set the value for a field, this abstration is here so that symbols and strings can be used for key values
   # param [String || Symbol] key the field to set
   # param [String] val the value to set for the field
@@ -88,12 +84,12 @@ class User < MongoBase
     key_sym = key.to_sym
     @attributes[key_sym] = value
   end
-  
+
   # Lock the user
   def lock
     set_attribute_value('locked',true)
   end
-  
+
   # Is this user currently locked
   def is_locked? 
     read_attribute_for_validation('locked') == true
@@ -103,13 +99,13 @@ class User < MongoBase
   def is_reset?
     read_attribute_for_validation( 'reset_key')
   end
-  
+
   # Merge the attributes with the record
   # @param [Hash] attributes the attributes to merge into this record
   def update(attributes)
     @attributes.merge!(attributes)
   end
-  
+
   #Save the user to the db, save only takes place if the record is valid based on the validation
   def save
     if valid?
@@ -121,22 +117,22 @@ class User < MongoBase
     end
     return false
   end
-  
+
   # Is this a new record, ie it has not been saved yet so there is no _id
   def new_record?
     _id.nil?
   end
-  
+
   # Remove the user from the db
   def destroy
     User.mongo['users'].remove(@attributes)
   end
-  
+
   # reload the user from the stored values in the db, this only works for saved records
   def reload
     unless new_record?
       @attributes = mongo['users'].find_one({'_id' => _id})
     end
-    
   end
+
 end
