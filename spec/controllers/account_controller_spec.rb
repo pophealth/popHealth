40da282a1b3f9b2a8eda2e4c993f7c1f1e0957bf
Andy Gregorowicz <andy@mitre.org>
2011-03-08 14:15:29 -0500

    Starting to test the account controller

---
+++ spec/controllers/account_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe AccountController do
+  it "should allow the creation of new accounts" do
+    controller.should_receive(:user=)
+    
+    post :create, :user => {:first_name => 'Joe', :last_name => 'Test', 
+                            :username => 'joetest', :password => 'sekret',
+                            :email => 'joe@test.org'}, 
+                  :agree_license => true, :password_confirmation => 'sekret'
+    
+    assigns(:user).valid?.should be_true
+    assigns(:user)._id.should_not be_nil
+    
+    assigns(:registration_errors).should be_empty
+  end
+  
+  it "should not allow an account to be created if it doesn't have a username" do
+    post :create, :user => {:first_name => 'Joe', :last_name => 'Test', 
+                            :password => 'sekret', :email => 'joe@test.org'}, 
+                  :agree_license => true, :password_confirmation => 'sekret'
+    
+    assigns(:user).valid?.should be_false
+    assigns(:user)._id.should be_nil
+    
+    assigns(:registration_errors).should_not be_empty
+  end
+end
