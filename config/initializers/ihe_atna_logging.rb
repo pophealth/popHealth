52c8c1427d70aad7749a02603f70094b62aa7510
Andy Gregorowicz <andy@mitre.org>
2011-05-20 10:53:45 -0400

    Adding ATNA logging back into login/logout actions using Warden hooks

--- config/initializers/ihe_atna_logging.rb
+++ config/initializers/ihe_atna_logging.rb
@@ -1,1 +1,16 @@
 require 'atna'
+
+Warden::Manager.after_authentication do |user,auth,opts|
+  Atna.log(user.username, :login)
+end
+
+Warden::Manager.before_failure do |env, opts|
+  request = Rack::Request.new(env)
+  attempted_login_name = request.params[:user].try(:[], :username)
+  attempted_login_name ||= 'unknown'
+  Atna.log(attempted_login_name, :login_failure)
+end
+
+Warden::Manager.before_logout do |user,auth,opts|
+  Atna.log(user.username, :logout)
+end
