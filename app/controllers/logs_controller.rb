0b0246bfcc044adfa49a7e36b26c25316c61b3ce
Andy Gregorowicz <andy@mitre.org>
2011-06-10 09:26:17 -0400

    Implementing basic sortability on the logs page by column

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,11 +1,26 @@
 class LogsController < ApplicationController
   before_filter :authenticate_user!
   
+  # All attributes of the Log class are valid to sort on except ones that start with an underscore.
+  VALID_SORTABLE_COLUMNS = Log.fields.keys.reject {|k| k[0] == '_'}
+  VALID_SORT_ORDERS = ['desc', 'asc']
+  
   def index
+    order = []
+    if VALID_SORTABLE_COLUMNS.include?(params[:sort]) && VALID_SORT_ORDERS.include?(params[:order])
+      order << [params[:sort].to_sym, params[:order].to_sym]
+    end
+
+    # If no valid order is specified, order by date
+    # If anything else is provided as a sort order, make date a secondary order
+    if order.empty? || order[0][0] != :created_at
+      order << [:created_at, :desc]
+    end
+
     if current_user.admin?
-      @logs = Log.paginate(:page => params[:page], :per_page => 20)
+      @logs = Log.order_by(order).paginate(:page => params[:page], :per_page => 20)
     else
-      @logs = Log.where(:username => current_user.username).paginate(:page => params[:page], :per_page => 20)
+      @logs = Log.where(:username => current_user.username).order_by(order).paginate(:page => params[:page], :per_page => 20)
     end
   end
 end
