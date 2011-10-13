68f7e2f4ff379df1c3e9af937d540469b3dd4699
Andre Quina <aquina@mitre.org>
2011-10-13 18:20:05 -0400

    initial test conversion

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,19 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'turn'
+
+require 'factory_girl'
+
+FactoryGirl.find_definitions
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+
+  def dump_database
+    User.all.each {|x| x.destroy}
+  end
+  
+  
 end
+
