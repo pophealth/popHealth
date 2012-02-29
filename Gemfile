205c1d1deeddf0820c934c1cb6c5aa0d989e9ce3
Andre Quina <aquina@mitre.org>
2012-02-29 11:53:14 -0500

    fixed issues with import

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '3.1.0'
 gem 'rack' , '1.3.3'
 
 #gem 'quality-measure-engine', '1.1.1'
-gem 'quality-measure-engine', :git => 'http://github.com/pophealth/quality-measure-engine.git', :branch => 'master'
+gem 'quality-measure-engine', :git => 'http://github.com/pophealth/quality-measure-engine.git', :branch => 'develop'
 #gem 'health-data-standards', '0.7.1'
 gem 'health-data-standards', :git => 'https://github.com/projectcypress/health-data-standards.git', :branch => 'develop'
 gem 'nokogiri'
--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -13,11 +13,7 @@ MONGO_DB.collection('patient_cache').ensure_index([['value.measure_id', Mongo::A
 base_fields = [['value.measure_id', Mongo::ASCENDING], ['value.sub_id', Mongo::ASCENDING], ['value.effective_date', Mongo::ASCENDING], ['value.test_id', Mongo::ASCENDING],  ['value.manual_exclusion', Mongo::ASCENDING]]
 
 %w(population denominator numerator antinumerator exclusions).each do |group|
-  MONGO_DB.collection('patient_cache').ensure_index(base_fields.clone.concat([["value.#{group}", Mongo::ASCENDING], ['value.gender', Mongo::ASCENDING]]), {name: "#{group}_gender"})
-  MONGO_DB.collection('patient_cache').ensure_index(base_fields.clone.concat([["value.#{group}", Mongo::ASCENDING], ['value.provider_performances.provider_id', Mongo::ASCENDING]]), {name: "#{group}_providers"})
-  MONGO_DB.collection('patient_cache').ensure_index(base_fields.clone.concat([["value.#{group}", Mongo::ASCENDING], ['value.race', Mongo::ASCENDING]]), {name: "#{group}_race"})
-  MONGO_DB.collection('patient_cache').ensure_index(base_fields.clone.concat([["value.#{group}", Mongo::ASCENDING], ['value.ethnicity', Mongo::ASCENDING]]), {name: "#{group}_ethnicity"})
-  MONGO_DB.collection('patient_cache').ensure_index(base_fields.clone.concat([["value.#{group}", Mongo::ASCENDING], ['value.languages', Mongo::ASCENDING]]), {name: "#{group}_language"})
+  MONGO_DB.collection('patient_cache').ensure_index(base_fields.clone.concat([["value.#{group}", Mongo::ASCENDING]]), {name: "#{group}_index"})
 end
 
 module QME
