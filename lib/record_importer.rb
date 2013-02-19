f91494c4cde7eebbdf599bd303b50f67b8277ba2
Andy Gregorowicz <andy@mitre.org>
2013-02-19 10:54:59 -0500

    Implementing the ability to post QRDA Cat I XML documents

--- lib/record_importer.rb
+++ lib/record_importer.rb
@@ -60,6 +60,8 @@ class RecordImporter
         patient_data = HealthDataStandards::Import::C32::PatientImporter.instance.parse_c32(doc)
       elsif doc.at_xpath("/cda:ClinicalDocument/cda:templateId[@root='2.16.840.1.113883.10.20.22.1.2']")
         patient_data = HealthDataStandards::Import::CCDA::PatientImporter.instance.parse_ccda(doc)
+      elsif doc.at_xpath("/cda:ClinicalDocument/cda:templateId[@root='2.16.840.1.113883.10.20.24.1.2']")
+        patient_data = HealthDataStandards::Import::Cat1::PatientImporter.instance.parse_cat1(doc)
       else
         STDERR.puts("Unable to determinate document template/type of CDA document")
         return {status: 'error', message: "Document templateId does not identify it as a C32 or CCDA", status_code: 400}
