cafeee0cba9f58b7b1ef7d40361172fa8e2bbfe3
Mark Yoon <yoon@northwestern.edu>
2013-08-27 15:02:09 -0500

    change reporting period in UI to 1 year. closes #1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,19 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
   before_filter :set_effective_date
   before_filter :check_ssl_used
-  
+
   add_breadcrumb APP_CONFIG['practice_name'], :root_url
-  
+
   # lock it down!
   check_authorization :unless => :devise_controller?
-  
+
   private
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     "#{Rails.configuration.relative_url_root}/logout.html"
   end
-  
+
   def hash_document
     d = Digest::SHA1.new
     checksum = d.hexdigest(response.body)
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
       "application"
     end
   end
-  
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to "#{root_url}403.html", :alert => exception.message
   end
@@ -47,13 +47,13 @@ class ApplicationController < ActionController::Base
     end
     @period_start = calc_start(@effective_date)
   end
-  
+
   def calc_start(date)
-    3.months.ago(Time.at(date))
+    1.year.ago(Time.at(date))
   end
-  
+
   def check_ssl_used
-    unless APP_CONFIG['force_unsecure_communications'] or request.ssl? 
+    unless APP_CONFIG['force_unsecure_communications'] or request.ssl?
       redirect_to "#{root_url}no_ssl_warning.html", :alert => "You should be using ssl"
     end
   end
