345a8aa6c81b371887942608dc6a232702b5130c
Liam Morley <liam@carpeliam.com>
2013-12-05 14:47:30 -0500

    set selected_measure_ids to [] if it's set to nil
    This works around a rails bug; see https://github.com/rails/rails/issues/8832 for more details

--- test/unit/models/user_test.rb
+++ test/unit/models/user_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+
   setup do
     dump_database
-    
+
     @user = Factory(:user)
   end
-  
+
   test "user should be found by username" do
     found_user = User.by_username @user.username
     assert_equal @user, found_user
@@ -17,5 +17,12 @@ class UserTest < ActiveSupport::TestCase
     found_user = User.by_email @user.email
     assert_equal @user, found_user
   end
-  
+
+  test "user preferences: selected_measure_ids replaces null with empty arrays" do
+    @user.preferences = {selected_measure_ids: nil}
+    assert @user.save
+    @user.reload
+    assert_equal [], @user.preferences['selected_measure_ids']
+  end
+
 end
