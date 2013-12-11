38febc837a4bed05ed4ce805de5a9a1b2c00bf3a
Andrew Hubley <ahubley@mitre.org>
2013-12-11 10:28:59 -0500

    more gemfile cleanup, libv8/therubyracer versions updated, removed kaminari dependency

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 PopHealth::Application.initialize!
 
-Provider.send(:include, Kaminari::MongoidExtension::Document)
-Record.send(:include, Kaminari::MongoidExtension::Document)
-
 require_relative '../lib/oid_helper'
 require_relative '../lib/hds/record.rb'
 require_relative '../lib/hds/provider.rb'
