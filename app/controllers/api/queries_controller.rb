5e3f73616b0a4a2a117e42ae8a53e6ab56f43e0e
Andy Gregorowicz <andy@mitre.org>
2014-02-14 13:18:53 -0500

    Documenting the QueriesController

--- app/controllers/api/queries_controller.rb
+++ app/controllers/api/queries_controller.rb
@@ -17,17 +17,30 @@ module Api
       render json: QME::QualityReport.where(filter)
     end
 
+    api :GET, '/queries/:id', "Retrieve clinical quality measure calculation"
+    param :id, String, :desc => 'The id of the quality measure calculation', :required => true
+    example '{"DENEX":0,"DENEXCEP":0,"DENOM":5,"IPP":5,"MSRPOPL":0,"NUMER":0,  "status":{"state":"completed", ...}, ...}'
+    description <<-SDESC
+      Gets a clinical quality measure calculation. If calculation is completed, the response will include the results.
+SDESC
     def show
       @qr = QME::QualityReport.find(params[:id])
       authorize! :read, @qr
       render json: @qr
     end
 
-    api :POST, '/queries', "Calculate a clinical quality measure"
+    api :POST, '/queries', "Start a clinical quality measure calculation"
     param :measure_id, String, :desc => 'The HQMF id for the CQM to calculate', :required => true
     param :sub_id, String, :desc => 'The sub id for the CQM to calculate. This is popHealth specific.', :required => false
     param :effective_date, Fixnum, :desc => 'Time in seconds since the epoch for the end date of the reporting period', 
                                    :required => true
+    example '{"_id":"52fe409bb99cc8f818000001", "status":{"state":"queued", ...}, ...}'
+    description <<-CDESC
+      This action will create a clinical quality measure calculation. If the measure has already been calculated,
+      it will return the results. If not, it will return the status of the calculation, which can be checked in
+      the status property of the returned JSON. If it is calculating, then the results may be obtained by the
+      GET action with the id. 
+CDESC
     def create
       build_filter
       authorize_providers
@@ -41,6 +54,8 @@ module Api
       render json: qr
     end
 
+    api :DELETE, '/queries/:id', "Remove clinical quality measure calculation"
+    param :id, String, :desc => 'The id of the quality measure calculation', :required => true
     def destroy
       qr = QME::QualityReport.find(params[:id])
       authorize! :delete, qr
@@ -48,6 +63,8 @@ module Api
       render :status=> 204, :text=>""
     end
 
+    api :PUT, '/queries/:id/recalculate', "Force a clinical quality measure to recalculate"
+    param :id, String, :desc => 'The id of the quality measure calculation', :required => true
     def recalculate
       qr = QME::QualityReport.find(params[:id])
       authorize! :recalculate , qr
@@ -56,6 +73,18 @@ module Api
       render json: qr
     end
 
+    api :GET, '/queries/:id/patient_results[?population=true|false]', 
+              "Retrieve patients relevant to a clinical quality measure calculation"
+    param :id, String, :desc => 'The id of the quality measure calculation', :required => true
+    param :ipp, /true|false/, :desc => 'Ensure patients meet the initial patient population for the measure', :required => false
+    param :denom, /true|false/, :desc => 'Ensure patients meet the denominator for the measure', :required => false
+    param :numer, /true|false/, :desc => 'Ensure patients meet the numerator for the measure', :required => false
+    param :denex, /true|false/, :desc => 'Ensure patients meet the denominator exclusions for the measure', :required => false
+    param :denexcp, /true|false/, :desc => 'Ensure patients meet the denominator exceptions for the measure', :required => false
+    param :msrpopl, /true|false/, :desc => 'Ensure patients meet the measure population for the measure', :required => false
+    param :antinumerator, /true|false/, :desc => 'Ensure patients are not in the numerator but are in the denominator for the measure', :required => false
+    param_group :pagination, Api::PatientsController
+    example '[{"_id":"52fe409ef78ba5bfd2c4127f","value":{"DENEX":0,"DENEXCEP":0,"DENOM":1,"IPP":1,"NUMER":1,"antinumerator":0,"birthdate":1276869600.0,"effective_date":1356998340.0,,"first":"Steve","gender":"M","last":"E","measure_id":"40280381-3D61-56A7-013E-6224E2AC25F3","medical_record_id":"ce83c561f62e245ad4e0ca648e9de0dd","nqf_id":"0038","patient_id":"52fbbf34b99cc8a728000068"}},...]'
     def patient_results
       qr = QME::QualityReport.find(params[:id])
       authorize! :read, qr
