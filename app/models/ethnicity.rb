0b3ccce53322d7312b99f232ef29b5ffe2f69d7c
Robert Clark <rbclark@mitre.org>
2014-03-27 14:44:08 -0400

    Update scopes to use procs in order for them to work properly with rails 4.

--- app/models/ethnicity.rb
+++ app/models/ethnicity.rb
@@ -6,7 +6,7 @@ class Ethnicity
   field :codes, type: Array
   
   scope :from_code, ->(code) {where("codes" => code)}
-  scope :ordered, order_by([:order, :asc])
+  scope :ordered, -> { asc(:order) }
   scope :selected, ->(ethnicity_ids) { any_in(:_id => ethnicity_ids)}
   scope :selected_or_all, ->(ethnicity_ids) { ethnicity_ids.nil? || ethnicity_ids.empty? ? Ethnicity.all : Ethnicity.selected(ethnicity_ids) }
   
