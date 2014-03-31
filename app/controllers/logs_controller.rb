6b22af0028349d9d269ae8e3903d111a41461d99
Andrew Hubley <ahubley@mitre.org>
2014-03-31 15:32:06 -0400

    removed legacy breadcrumb code, modified header erb to pull root provider name if available

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,13 +1,12 @@
 class LogsController < ApplicationController
-  
+
   before_filter :authenticate_user!
   before_filter :validate_authorization!
-  add_breadcrumb 'access logs', '/logs'
-  
+
   # All attributes of the Log class are valid to sort on except ones that start with an underscore.
   VALID_SORTABLE_COLUMNS = Log.fields.keys.reject {|k| k[0] == '_'}
   VALID_SORT_ORDERS = ['desc', 'asc']
-  
+
   def index
     order = []
     if VALID_SORTABLE_COLUMNS.include?(params[:sort]) && VALID_SORT_ORDERS.include?(params[:order])
@@ -19,28 +18,28 @@ class LogsController < ApplicationController
     if order.empty? || order[0][0] != :created_at
       order << [:created_at, :desc]
     end
-    
+
     where = {}
     where[:username] = current_user.username unless current_user.admin?
-    
+
     start_date = date_param_to_date(params[:log_start_date])
     if start_date
       where[:created_at] = {'$gte' => start_date}
     end
-    
+
     end_date = date_param_to_date(params[:log_end_date])
     if end_date
       # will create an empty hash if created_at is nil or leave start_date alone if it is there
       where[:created_at] ||= {}
       where[:created_at].merge!('$lt' => end_date.next_day) # becomes less than midnight the next day
     end
-    
+
     @logs = Log.where(where).order_by(order).paginate(:page => params[:page], :per_page => 20)
 
   end
-  
+
   private
-  
+
   def date_param_to_date(date_string)
     if date_string && date_string.split('/').length == 3
       split_date = date_string.split('/').map(&:to_i)
@@ -49,9 +48,9 @@ class LogsController < ApplicationController
       nil
     end
   end
-  
+
   def validate_authorization!
     authorize! :read, Log
   end
-  
+
 end
