623e1680d760ce374dea5d44546f78f738898391
Robert Clark <rbclark@mitre.org>
2014-04-10 13:13:11 -0400

    Fixed bundle importer to embed many cda identifiers. (Was causing an error with the bundle importer)

--- lib/hds/provider.rb
+++ lib/hds/provider.rb
@@ -2,6 +2,8 @@ class Provider
 
   field :level, type: String
   
+  embeds_many :cda_identifiers, class_name: "CDAIdentifier"
+
   scope :alphabetical, ->{order_by([:family_name, :asc], [:given_name, :asc])}
   scope :can_merge_with, ->(prov) { prov.npi.blank? ? all_except(prov) : all_except(prov).without_npi }
   scope :all_except, ->(prov) { where(:_id.ne => prov.id) }
