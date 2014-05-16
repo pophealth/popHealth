7ff5063a760a92d842ae8073f23be4447f47eb5b
Andrew Schreiber <aschreiber@va00029-pc.mitre.org>
2014-05-16 13:09:20 -0400

    Created rake task to export CAT3 by provider for selected measures and year.
    It prints it to stdout.

---
+++ app/helpers/reports_helper.rb
@@ -0,0 +1,26 @@
+module ReportsHelper
+  def generate_cat3(provider, effective_date, measure_ids)
+    exporter = HealthDataStandards::Export::Cat3.new
+    effective_date ||= Time.gm(2012,12,31)
+    end_date = DateTime.new(effective_date.to_i, 12, 31)
+    provider_filter = {}
+    provider_filter['filters.providers'] = provider.id
+    filter = measure_ids==["all"] ? {} : {:cms_id.in => measure_ids}
+    return exporter.export(HealthDataStandards::CQM::Measure.top_level.where(filter),
+                            generate_header(provider),
+                            effective_date.to_i,
+                            end_date.years_ago(1),
+                            end_date, provider_filter)
+  end
+
+  def generate_header(provider)
+    header = Qrda::Header.new(APP_CONFIG["cda_header"])
+
+    header.identifier.root = UUID.generate
+    header.authors.each {|a| a.time = Time.now}
+    header.legal_authenticator.time = Time.now
+    header.performers << provider
+
+    header
+  end
+end
