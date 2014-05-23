b9e33c9cd93208044d9df482ba83b7b0d765197b
Robert Clark <rbclark@mitre.org>
2014-05-23 14:05:51 -0400

    Add secret_key_base since not adding it causes depreciation errors in rails 4.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 PopHealth::Application.config.secret_token = 'd2d8152dfb0736f5ae682c88a0e416c0104b9c0271ac9834d7349653c96f38715b94fc9bad48641725b149a83e59e77d0e7554cd407bb1771f9d343ce217c596'
+
+PopHealth::Application.config.secret_key_base = '3e393dd55e17e1836ea8c7feb65595d120bc2f23a58e811f1e2256dc6de615704b99c964f4149c33b4728f566a16bde4bae2a1eecc49fe7715057de4932f0f8f'
