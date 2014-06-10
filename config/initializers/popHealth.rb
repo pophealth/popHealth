82f57b750486f8595c44d703185a5b576ef2aa52
Andrew Hubley <ahubley@mitre.org>
2014-06-10 10:11:15 -0400

    fixed language json file to support 3 letter codes, re-added code to load it into mongo

--- config/initializers/popHealth.rb
+++ config/initializers/popHealth.rb
@@ -1,3 +1,10 @@
 require 'hqmf-parser'
 
 APP_CONFIG = YAML.load_file(Rails.root.join('config', 'popHealth.yml'))[Rails.env]
+
+# insert languages
+(
+  JSON.parse(File.read(File.join(Rails.root, 'test', 'fixtures', 'code_sets', 'languages.json'))).each do |document|
+    MONGO_DB['languages'].insert(document)
+  end
+) if MONGO_DB['languages'].find({}).count == 0
