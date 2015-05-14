a26d36ec1885d549416104f5c7e8b1dcf7e0aea8
Sabbir Siddiqui <ssiddiqui@smcpartners.com>
2015-05-13 22:44:38 -0400

    fixed practice create api

--- app/controllers/api/practices_controller.rb
+++ app/controllers/api/practices_controller.rb
@@ -27,9 +27,10 @@ module Api
     api :POST, "/practices", "Get the practice information"
     param :name, String, :desc => "Practice Name", :required => true
     param :organization, String, :desc => "Practice organization", :required => true
+    param :user, String, :desc => "User to assign to practice", :required => false
     formats ['json']
     def create
-      @practice = Practice.new(:name => params[:name], :organization => params[:organization])
+      @practice = Practice.create(:name => params[:name], :organization => params[:organization])
 
       if @practice.save!
         identifier = CDAIdentifier.new(:root => "Organization", :extension => @practice.organization)
@@ -39,11 +40,12 @@ module Api
         provider.save
         @practice.provider = provider
         
-        if params[:user] != ''
+        if params[:user] != '' && params[:user]
           user = User.find(params[:user])
           @practice.users << user
           user.save
         end
+        @practice.save!
       else
         @practice = nil
       end
