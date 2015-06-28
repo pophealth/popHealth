4e633aba320c6b114bce05bd0473035cc0d2735d
Sabbir Siddiqui <ssiddiqui@smcpartners.com>
2015-06-28 15:14:50 -0400

    added preference and views

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -6,5 +6,6 @@ class Preference
   field :should_display_circle_visual, type: Boolean, default: true
   field :population_chart_scaled_to_IPP, type: Boolean, default: false
   field :should_display_provider_tree, type: Boolean, default: false
+  field :show_aggregate_result, type: Boolean, default: false
   belongs_to :user
 end
