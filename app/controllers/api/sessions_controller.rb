4e50b3cd1f5d293a8cd1c01b1aa3db9c477beb53
Chris Lohl <chris.lohl@cgm.com>
2016-09-25 20:56:52 -0700

    try to redirect after sign on
    Signed-off-by: Chris Lohl <chris.lohl@cgm.com>

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,10 +10,25 @@ module Api
     def check_auth
       authenticate_or_request_with_http_basic do |username,password|
         resource = User.by_username(username)
+        return invalid_login_attempt unless resource
+
         if resource.valid_password?(password)
           sign_in :user, resource
+
+          if (providerId) 
+            store_location_for(resource, "/#providers/" + providerId) 
+          else
+            render :json=>{:success=>false, :message=>"User is missing provider id"}, :status=>422
+          end
+
         end
+
       end
+      
+    end
+
+    def invalid_login_attempt
+      render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
     end
 
   end
